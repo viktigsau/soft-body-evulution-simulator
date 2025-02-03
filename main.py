bf2fd71ec95d1a6def1723eaab889f0935ae0fc6
viktigsau <elmerbleiktvedt@hotmail.com>
2025-02-03 18:05:57 +0100

    rtf6yijftg7yiy7iy7iyui

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+from classes import *
+
+
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+
+entitys = [
+
+]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    
+    window.fill((0, 0, 0))
+
+    for individual in entitys:
+        individual.update()
+    
+    pygame.display.flip()
